27a91f89dba97875cf1201c051405e8afbb4e111
Marcia Alegria <ytasegundo@gmail.com>
2023-04-08 22:29:17 -0400

    barra progreso and controlls

--- index.js
+++ index.js
@@ -25,13 +25,37 @@ const songList= [
         cover: "newkids1.png"
     },
 ]
+
+//cancion actual
+let actualSong= null
 //captura elementos dom
  const songsul = document.getElementById("s");
  const audio = document.getElementById("audio");
  const cover = document.getElementById("cover");
- const title = document.getElementById("title")
+ const title = document.getElementById("title");
+ const play = document.getElementById("play");
+ const prev = document.getElementById("back");
+ const next = document.getElementById("next");
+ const progress = document.getElementById("progress")
+ const progressContainer = document.getElementById("progressContainer")
+ 
+ progressContainer.addEventListener("click", setProgress)
 
+ // 
+  audio.addEventListener("timeupdate", updateProgress)
+
+// escuchar click en el controles
+play.addEventListener("click", ()=> {
+    
+    if(audio.paused){
+        playSong()
+    } else {
+        pauseSong()
+    }
+})
 //funcion para cargar canciones y mostrar listado
+next.addEventListener("click", ()=> nextSong())
+prev.addEventListener("click", ()=> prevSong())
 
 function loadSongs() {
     songList.forEach((song, index) => {
@@ -44,23 +68,97 @@ function loadSongs() {
         link.addEventListener("click", () => loadSong(index))
         li.appendChild(link)
         songsul.appendChild(li)
-
+        link.style.listStyle= "none"
+        link.style.textDecorationLine="none"
+        link.style.color ="white"
     })
 }
 
 //cargar canción seleccionada
 function loadSong(songIndex) {
-    console.log(songIndex);
-    audio.src = "./music/" + songList[songIndex].file
-    audio.play()
-    changeCover(songIndex)
-    changeSongTitle(songIndex)
+    if(songIndex != actualSong){
+        changeActiveClass(actualSong, songIndex)      
+        actualSong = songIndex
+        audio.src = "./music/" + songList[songIndex].file
+        playSong()
+        changeCover(songIndex)  
+    }
 }
+// barra progreso
+function updateProgress(event){
+    //total y actual 
+    const {duration, currentTime} = event.srcElement
+    const percent = (currentTime / duration) * 100
+    progress.style.width= percent + "%"
+}
+function setProgress(event){
+    const totalWidth = this.offsetWidth
+    const progressWidht= event.offsetX
+    const current = (progressWidht / totalWidth) * audio.duration
+    audio.currentTime = current
+}
+function updateControls() {
+    if(audio.paused){
+        play.classList.remove("fa-pause")
+        play.classList.add("fa-play")
+    } else {
+        play.classList.add("fa-pause")
+        play.classList.remove("fa-play")
+    }
+}
+
+function playSong(){
+    if(actualSong != null){
+     audio.play()
+    updateControls()   
+    }
+   
+}
+
+function pauseSong() {
+    if (actualSong != null) {
+        audio.pause()
+        updateControls()
+        
+    }
+}
+function changeActiveClass(lastIndex, newIndex){
+    const links = document.querySelectorAll("a")
+    if(lastIndex != null){
+        links[lastIndex].classList.remove("activo")
+
+    }
+    links[newIndex].classList.add("activo")
+}   
+
 function changeCover(songIndex){
     cover.src = "./img/" + songList[songIndex].cover
-
 }
 function changeSongTitle(songIndex){
     title.innerText = songList[songIndex].title
 }
+
+//anterior cancion
+function prevSong() {
+    if (actualSong > 0) {
+
+        loadSong(actualSong -1)
+        
+    } else {
+        loadSong(songList.length -1)
+    }
+    }
+
+//siguiente cancion
+function nextSong() {
+    if (actualSong < songList.length -1) {
+        loadSong(actualSong + 1)
+        
+    }else{
+        loadSong(0)
+    }
+    }
+//siguiente canción al terminar la actual
+audio.addEventListener("ended", ()=> nextSong())
+
 loadSongs()
